c068f37690101195926fc3988fe467cf7f6ef5d5
Enzo Huaricapcha <lhuaricapcha@gmail.com>
2017-08-01 15:06:51 -0500

    Feat: change api url

--- app/src/main/java/com/mixturadesabores/junin/lacarta/data/RestApi.kt
+++ app/src/main/java/com/mixturadesabores/junin/lacarta/data/RestApi.kt
@@ -3,16 +3,13 @@ package com.mixturadesabores.junin.lacarta.data
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-/**
- * Created by Enzo Huaricapcha on 12/07/2017.
- */
 open class RestApi() {
 
     val sailsApi: SailsApi
 
     init {
         val retrofit = Retrofit.Builder()
-                .baseUrl("http://192.168.1.39:1337/")
+                .baseUrl("http://192.168.1.40:1337/")
                 .addConverterFactory(MoshiConverterFactory.create())
                 .build()
 
